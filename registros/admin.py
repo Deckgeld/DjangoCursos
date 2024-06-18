3b50eaf5fc6b3a0c8856446199cb65bd4cffe8c8
Tu nombre <utm210008ti@ut-morelia.edu.mx>
2024-06-17 23:16:32 -0600

    create models and modify titles

---
+++ registros/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Cursos
+
+# Register your models here.
+class AdministrarModelo(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Cursos, AdministrarModelo)
+
---
+++ registros/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Cursos(models.Model): #Define la estructura de nuestra tabla
+    id = models.PositiveIntegerField(primary_key=True) #Número entero positivo
+    nombre = models.TextField() #Texto largo
+    categoria = models.TextField()
+    autor = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2,) #Número con decimales
+    likes = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True) #Fecha y tiempo
+    updated = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Curso"
+        verbose_name_plural = "Cursos"
+        ordering = ["-created"]
+        #el menos indica que se ordenara del más reciente al mas viejo
+    
+    def __str__(self):
+        return self.nombre
+        #Indica que se mostrára el nombre como valor en la tabla
